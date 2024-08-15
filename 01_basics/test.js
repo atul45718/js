606618dd4b7d76962b4b302dce0b68173c88fbba
atul45718 <atulraju5718@gmail.com>
2024-08-15 10:23:23 +0000

    container confi-2

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,8 @@
 console.log("Hello Atul")
+
+const userId=12345
+let userName="Atul"
+let userEmail="Atul@gmail.com"
+let userNumber=91-8381997522
+accountCity="Noida";
+console.table([userId,userName,userEmail,userNumber,accountCity]);
