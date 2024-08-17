d452d93642bffabd077ba5639b1dda78bab18fda
atul45718 <atulraju5718@gmail.com>
2024-08-17 15:14:14 +0000

    comprison-second-file-save

--- 01_basics/02_comparison.js
+++ 01_basics/02_comparison.js
@@ -1,10 +1,14 @@
-console.log("20" > 1);
-console.log("02" > 1);
+// console.log("20" > 1);
+// console.log("02" > 1);
 
 
-console.log(null>0);
-console.log(null==>0);
-console.log(null>=0);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined===0);
+console.log(undefined > 0);
 
 
-
+//Do not these  type of comparison //
