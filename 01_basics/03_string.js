5d0a4934b3f9256604533828a7f31e4c5bf5bec2
atul45718 <atulraju5718@gmail.com>
2024-08-19 07:15:32 +0000

    string-2

--- 01_basics/03_string.js
+++ 01_basics/03_string.js
@@ -3,4 +3,25 @@ const repoCount = 50
 //console.log(name + repoCount + "value");
 console.log(`Hello my name is ${name} and my repo count is${repoCount}`);
 
-const gameName = new String('Atulkumar')
+const gameName = new String('Atulkumar-ak')
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+//console.log(newString);
+
+const anotherString =  gameName.slice(-1, 3) // it is use to reverse the value 
+console.log(anotherString);
+  const newStringOne = "  atul  "
+  console.log(newStringOne);
+  console.log(newStringOne.trim()); // remove the  white space form bith side of string bcz transfer less data in database.
+
+  const url = "https://atul.com/atul%20kumar" //  remove the white space b/w the string like ex: atul kumar => boswer look like this  atul%20kumar=> replace('%20 , ') => atul_kumar
+ 
+  console.log(url.replace('%20' , '_'))
+  console.log(url.includes('atul'));
+  
+  
