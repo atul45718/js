f03955a7cec0fc8cf399f679869e51adfba7b3bf
atul45718 <atulraju5718@gmail.com>
2024-08-20 05:41:13 +0000

    string-methos-1

---
+++ 01_basics/string_method.js
@@ -0,0 +1,24 @@
+// String is object it is use to reperesents and manipulates the sequences of  the characters.
+// it's can be written by three ways 1-single quotes (''), 2-double quotes (""), 3-backsticks (``)
+// it can be useful to holding the data that can be repersented in text form .
+ const str1 = "A String is primitive "
+ const str2 = 'It will also primitive'
+ const str3 = `yet another string primitive` // also know as template string 
+
+//  console.log(typeof str1); //string
+//  console.log(typeof str2);//string
+//  console.log(typeof str3);//string
+ 
+
+ // one more ways written string  call new keyword - str4 = new string() but it will be objects types 
+
+ const str4 = new String("Welcome to javascript")
+
+ //console.log(typeof str4);//object
+ 
+ // string lenth - to find the length of string .it is use to be length property.
+
+let  newString = "Vikings"
+//console.log(newString.length);
+
+// String Methods
