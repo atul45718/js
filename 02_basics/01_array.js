1e02779fbb4262e383344f44349b7850f4d78d1d
atul45718 <atulraju5718@gmail.com>
2024-09-06 03:00:51 +0000

    array_4

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -24,7 +24,7 @@ const myArr2 = new Array(1,2,3)
 // console.log( typeof newArr);
 
 // slice , splice (interviews Questions arise is to be Diff.between slice and splice => slice can change the copy array but splice can changes in orginal array)
-
+/*
 console.log("A", myArr);
 const myn1 = myArr.slice(1,3)
 
@@ -33,4 +33,38 @@ console.log("B", myArr);
 
 const myn2 = myArr.splice(1,3)
 console.log("C", myArr);
-console.log(myn2);
+console.log(myn2); */
+
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// Concat()method :- cobination of two or more methods and retuns always new array.
+
+// const all_heros = marvel_heros.concat(dc_heros)
+// console.log(all_heros);
+
+// sperade operator :-https://www.freecodecamp.org/news/how-to-merge-arrays-in-javascript-array-concatenation-in-js/
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros);
+
+// flat() method :-The flat() method of Array instances creates a new array with all sub-array elements concatenated into it recursively up to the specified depth. 
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+// from() Method:- An iterable or array-like object to convert to an array.
+console.log(Array.isArray("Atul"));
+console.log(Array.from("Atul"));
+console.log(Array.from({name:"atul"})); // insteresting :- for intervinterviews 
+
+//Array.of() method :-The Array.of() static method creates a new Array instance from a variable number of arguments, regardless of number or type of the arguments. 
+let score1= "atul"
+let score2=200
+let score3=300
+console.log(Array.of(score1, score2, score3));
